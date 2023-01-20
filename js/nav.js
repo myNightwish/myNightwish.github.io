50d3c86068bf4ba96d077a266b5ee14633e45cc1
myNightwish <zml_byr@bupt.edu.cn>
2023-01-20 20:12:29 +0800

    Site updated: 2023-01-20 20:12:28

---
+++ js/nav.js
@@ -0,0 +1,32 @@
+document.addEventListener('pjax:complete', tonav);
+document.addEventListener('DOMContentLoaded', tonav);
+//响应pjax
+function tonav() {
+    document.getElementById("name-container").setAttribute("style", "display:none");
+    var position = $(window).scrollTop();
+    $(window).scroll(function () {
+        var scroll = $(window).scrollTop();
+        if (scroll > position) {
+            document.getElementById("name-container").setAttribute("style", "");
+            document.getElementsByClassName("menus_items")[1].setAttribute("style", "display:none!important");
+        } else {
+            document.getElementsByClassName("menus_items")[1].setAttribute("style", "");
+            document.getElementById("name-container").setAttribute("style", "display:none");
+        }
+        position = scroll;
+    });
+    //修复没有弄右键菜单的童鞋无法回顶部的问题
+    const titleInfo = document.title.split('| Nightwishのblog🧊');
+    if(titleInfo.length >= 2) {
+        document.getElementById("page-name").innerText = titleInfo[0];
+    } 
+}
+
+function scrollToTop() {
+    document.getElementsByClassName("menus_items")[1].setAttribute("style", "");
+    document.getElementById("name-container").setAttribute("style", "display:none");
+    btf.scrollToDest(0, 500);
+}
+
+
+
