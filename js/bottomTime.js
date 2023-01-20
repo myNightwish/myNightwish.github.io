4b955e50b2c6802dbdff96a7c885cc4a8422ba99
myNightwish <zml_byr@bupt.edu.cn>
2023-01-20 22:39:11 +0800

    Site updated: 2023-01-20 22:39:10

--- js/bottomTime.js
+++ js/bottomTime.js
@@ -1,17 +1,1 @@
-var create_time= Math.round(new Date("2021-12-20 20:14:00").getTime()/1000);
-var timestamp=Math.round((new Date().getTime()+8*60*60*1000)/1000);
-var second=timestamp-create_time;
-var time=new Array(0,0);
-if(second>=365*24*3600){
-  time[0]=parseInt(second/(365*24*3600));
-  second %=365*24*3600;
-}
-if(second>=24*3600){
-  time[1]=parseInt(second/(24*3600));
-  second %=24*3600;
-}
-var currentTimeHtml='小破站已经安全运行 '+time[0]+' 年 '+time[1]+' 天 ';
-var elementById=document.getElementById('running-time');
-if(elementById){
-  elementById.innerHTML=currentTimeHtml;
-}
+var create_time=Math.round(new Date("2021-12-20 20:14:00").getTime()/1e3),timestamp=Math.round(((new Date).getTime()+288e5)/1e3),second=timestamp-create_time,time=new Array(0,0);second>=31536e3&&(time[0]=parseInt(second/31536e3),second%=31536e3),second>=86400&&(time[1]=parseInt(second/86400),second%=86400);var currentTimeHtml="小破站已经安全运行 "+time[0]+" 年 "+time[1]+" 天 ",elementById=document.getElementById("running-time");elementById&&(elementById.innerHTML=currentTimeHtml);
